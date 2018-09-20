5ca3f351ad0d3376398cb2d7225dae58c263e83b
noguchi takeshi <panda531531@gmail.com>
2018-09-20 23:31:07 +0900

    必要なファイルを追加してアニメーション実行時にエラーが再現するとこまで完了

---
+++ XamarinFormsAnimationSample/Triggers/LabelFontSizeTriggerAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFormsAnimationSample.Triggers
+{
+	public class LabelFontSizeTriggerAction : TriggerAction<VisualElement>
+	{
+		public int FontSize { get; set; }
+
+		public LabelFontSizeTriggerAction()
+		{
+		}
+
+		protected override void Invoke(VisualElement sender)
+		{
+			var animation = new Animation((d) =>
+			{
+				var label = sender as Label;
+				label.FontSize = FontSize;
+			});
+			sender.Animate("", animation, length: 1000, easing: Easing.Linear);
+		}
+	}
+}
---
+++ XamarinFormsAnimationSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace XamarinFormsAnimationSample
+{
+	public class MainPageViewModel : BindableBase
+	{
+		#region BindableProperty
+		private bool _IsAnimationStart;
+		public bool m_IsAnimationStart
+		{
+			get { return _IsAnimationStart; }
+			set { this.SetProperty(ref this._IsAnimationStart, value); }
+		}
+		#endregion
+
+		#region Commands
+		public DelegateCommand StartAnimationCommand { get; }
+		#endregion
+
+		public MainPageViewModel()
+		{
+			// Create Commands
+			StartAnimationCommand = new DelegateCommand(StartAnimation);
+		}
+
+		private void StartAnimation()
+		{
+			m_IsAnimationStart = true;
+
+			// １秒待ってfalseにする
+			Task.Run(() =>
+			{
+				Thread.Sleep(1000);
+			}).ContinueWith((r) =>
+			{
+				m_IsAnimationStart = false;
+			});
+		}
+	}
+}
