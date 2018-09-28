79e1d8e9bf92a9e4cc534c8cde6f11f699a2cd27
noguchi takeshi <panda531531@gmail.com>
2018-09-28 09:24:59 +0900

    VisualElementのOpacityをアニメーションするTriggerActionを追加

--- XamarinFormsAnimationSample/Triggers/VisualElement/OpacityTriggerAction.cs
+++ XamarinFormsAnimationSample/Triggers/VisualElement/OpacityTriggerAction.cs
@@ -1,7 +1,21 @@
 ﻿using System;
-namespace XamarinFormsAnimationSample
+using Xamarin.Forms;
+
+namespace XamarinFormsAnimationSample.Triggers
 {
-	public interface OpacityTriggerAction
+	public class OpacityTriggerAction : TriggerAction<VisualElement>
 	{
+		public int StartsFrom { set; get; }
+
+		protected override void Invoke(VisualElement sender)
+		{
+			sender.Animate("VisualElementOpacityAnimation", new Animation((d) =>
+			{
+				var animationRatio = StartsFrom == 0 ? d : 1 - d;
+				sender.Opacity = animationRatio;
+			}),
+			length: 1000, // milliseconds
+			easing: Easing.Linear);
+		}
 	}
 }
