ab241d9ead8b086b376610cb40eff2a666b5042a
noguchi takeshi <panda531531@gmail.com>
2018-10-01 23:19:16 +0900

    * MainPage.xaml: アニメーションに合わせて画面変更
    * ChangeLabelTextColorTriggerAction.cs: 名前変更
    
    * LabelFontSizeDoubleAnimation.cs: 名前変更
    LengthとEasingのプロパティの初期値を指定
    Easingはコンバーターで変換した値を入れるように変更
    
    * LabelFontSizeTriggerAction.cs: 名前変更による削除
    
    * EasingValueConverter.cs:
    * XamarinFormsAnimationSample.csproj:
      Easing関数指定を文字列で行って変換するようのコンバータークラスを追加

---
+++ XamarinFormsAnimationSample/Triggers/Label/LabelFontSizeDoubleAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using XamarinFormsAnimationSample.Utils;
+
+namespace XamarinFormsAnimationSample.Triggers
+{
+	public class LabelFontSizeDoubleAnimation : TriggerAction<VisualElement>
+	{
+		// Animation Parameter
+		public double From { get; set; }
+		public double To { get; set; }
+		public int StartsFrom { set; get; }
+		public uint Length { get; set; } = 1000;
+		public string Easing { get; set; } = "Linear";
+
+		private double gap;
+
+		public LabelFontSizeDoubleAnimation() { }
+
+		/// <summary>
+		/// Invoke change font-size animation.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		protected override void Invoke(VisualElement sender)
+		{
+			gap = CalculateGap(From, To);
+
+			var animation = new Animation((d) =>
+			{
+				var animationRatio = StartsFrom == 0 ? d : 1 - d;
+				var currentSize = gap * animationRatio;
+				(sender as Label).FontSize = From + currentSize;
+			});
+			sender.Animate("LabelFontSize", animation, length: Length, easing: EasingValueConverter.Convert(Easing));
+		}
+
+		/// <summary>
+		/// Calculates the font-size gap.
+		/// </summary>
+		/// <returns>The gap.</returns>
+		/// <param name="from">From.</param>
+		/// <param name="to">To.</param>
+		private double CalculateGap(double from, double to)
+		{
+			return to - from;
+		}
+	}
+}
