dbf14867d3c0abbeefc9eb317d175492c26f93da
noguchi takeshi <panda531531@gmail.com>
2018-10-01 23:58:17 +0900

    * MainPage.xaml: * BackgroundColorAnimation.cs: * ChangeBackgroundColorTriggerAction.cs: クラス名変更
    * ChangeLabelTextColorTriggerAction.cs: プロパティの初期化追加

--- XamarinFormsAnimationSample/Triggers/Label/ChangeLabelTextColorTriggerAction.cs
+++ XamarinFormsAnimationSample/Triggers/Label/ChangeLabelTextColorTriggerAction.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.IO;
 using Xamarin.Forms;
+using XamarinFormsAnimationSample.Utils;
 
 namespace XamarinFormsAnimationSample.Triggers
 {
 	public class ChangeLabelTextColorTriggerAction : TriggerAction<VisualElement>
 	{
-		public int StartsFrom { set; get; }
+		// Animation Parameter
 		public Color From { set; get; }
 		public Color To { set; get; }
+		public int StartsFrom { set; get; }
+		public uint Length { get; set; } = 1000;
+		public string Easing { get; set; } = "Linear";
 
 		// FromとToの差を保持する
 		private double _RGap;
@@ -26,15 +30,14 @@ namespace XamarinFormsAnimationSample.Triggers
 
 			sender.Animate("ChangeLabelTextColor", new Animation((d) =>
 			{
-				var percent = StartsFrom == 0 ? d : 1 - d;
-				var rVal = _RGap * percent;
-				var gVal = _GGap * percent;
-				var bVal = _BGap * percent;
-				var label = sender as Label;
-				label.TextColor = Color.FromRgb(From.R + rVal, From.G + gVal, From.B + bVal);
+				var animationRatio = StartsFrom == 0 ? d : 1 - d;
+				var rVal = _RGap * animationRatio;
+				var gVal = _GGap * animationRatio;
+				var bVal = _BGap * animationRatio;
+				(sender as Label).TextColor = Color.FromRgb(From.R + rVal, From.G + gVal, From.B + bVal);
 			}),
-			length: 1000, // milliseconds
-			easing: Easing.Linear);
+			length: Length,
+			easing: EasingValueConverter.Convert(Easing));
 		}
 
 		/// <summary>
---
+++ XamarinFormsAnimationSample/Triggers/VisualElement/BackgroundColorAnimation.cs
@@ -0,0 +1,51 @@
+﻿// Official Sample
+// https://docs.microsoft.com/ja-jp/xamarin/xamarin-forms/app-fundamentals/triggers#enterexit
+
+using System;
+using Xamarin.Forms;
+using XamarinFormsAnimationSample.Utils;
+
+namespace XamarinFormsAnimationSample.Triggers
+{
+	public class BackgroundColorAnimation : TriggerAction<VisualElement>
+	{
+		// Animation Parameter
+		public Color From { get; set; }
+		public Color To { get; set; }
+		public int StartsFrom { set; get; }
+		public uint Length { get; set; } = 1000;
+		public string Easing { get; set; } = "Linear";
+
+		// FromとToの差を保持する
+		private double rGap;
+		private double gGap;
+		private double bGap;
+
+		protected override void Invoke(VisualElement sender)
+		{
+			CalculateGap(From, To);
+
+			sender.Animate("BackgroundColor", new Animation((d) =>
+			{
+				var animationRatio = StartsFrom == 0 ? d : 1 - d;
+				var rVal = rGap * animationRatio;
+				var gVal = gGap * animationRatio;
+				var bVal = bGap * animationRatio;
+				sender.BackgroundColor = Color.FromRgb(From.R + rVal, From.G + gVal, From.B + bVal);
+			}),
+			length: Length,
+			easing: EasingValueConverter.Convert(Easing));
+		}
+
+		/// <summary>
+		/// Calculates the gap between From and To.
+		/// </summary>
+		private void CalculateGap(Color from, Color to)
+		{
+			rGap = to.R - from.R;
+			gGap = to.G - from.G;
+			bGap = to.B - from.B;
+		}
+
+	}
+}
