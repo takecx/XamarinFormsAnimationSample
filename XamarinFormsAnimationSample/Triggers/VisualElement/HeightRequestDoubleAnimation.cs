1b235cb797b81426c59cd7c828470535a59a11e4
noguchi takeshi <panda531531@gmail.com>
2018-10-02 17:44:09 +0900

    * HeightRequestDoubleAnimation.cs:   VisualElementのHeightRequest用のアニメーション追加
    * MainPage.xaml: 修正

---
+++ XamarinFormsAnimationSample/Triggers/VisualElement/HeightRequestDoubleAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using XamarinFormsAnimationSample.Interfaces;
+using XamarinFormsAnimationSample.Utils;
+
+namespace XamarinFormsAnimationSample.Triggers
+{
+	public class HeightRequestDoubleAnimation : TriggerAction<VisualElement>, ITriggerAction<double>
+	{
+		// Animation Parameter
+		public double From { get; set; }
+		public double To { get; set; }
+		public int StartsFrom { set; get; }
+		public uint Length { get; set; } = 1000;
+		public string Easing { get; set; } = "Linear";
+
+		public double CalculateGap(double from, double to)
+		{
+			return to - from;
+		}
+
+		protected override void Invoke(VisualElement sender)
+		{
+			var gap = CalculateGap(From, To);
+			sender.Animate("HeightRequestDoubleAnimation", new Animation((d) =>
+			{
+				var animationRatio = StartsFrom == 0 ? d : 1 - d;
+				var currentValue = gap * animationRatio;
+				sender.HeightRequest = From + currentValue;
+			}),
+			length: Length,
+			easing: EasingValueConverter.Convert(Easing));
+		}
+	}
+}
